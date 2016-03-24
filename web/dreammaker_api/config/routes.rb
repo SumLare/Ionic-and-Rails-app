d16a4eee94ee64e5271b33c20b94aabdac2def92
SumLare <mrhard95@gmail.com>
2016-03-24 12:23:24 +0300

    Working with devise

--- web/dreammaker_api/config/routes.rb
+++ web/dreammaker_api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   scope module: 'api', defaults: { format: :json } do
     namespace :v1 do
-      resources :dreams, except: [:new]
+      resources :dreams
     end
   end
 
